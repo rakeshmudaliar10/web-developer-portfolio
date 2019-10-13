9d822349619212dcee9695992c6353cd08e02407
rakesh <rakeshmudaliar10@gmail.com>
2019-10-13 16:22:29 +0530

    portfolio website 1st

---
+++ js/my-js.js
@@ -0,0 +1,75 @@
+/*!
+ * Bootstrap v3.3.4 (http://getbootstrap.com)
+
+ */
+function ($) {
+ "use strict";
+
+ $('.navbar-nav>li').on( 'click', function(){
+  var my-active = $(this);
+  $(this).siblings().removeClass('active');
+  my-active.addClass('active');
+
+ });
+
+  var topoffset = 50; //variable for menu height
+
+  $('body').scrollSpy({
+
+    target: 'header .navbar';
+    offset: topoffset
+  });
+
+  // add inbody class
+  var hash = $(this).find('li.active a').attr('href');
+  if(hash !== '#featured') {
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+
+
+  // Add an inbody class to nav when scrollspy event fires
+  $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+      $('header nav').addClass('inbody');
+    } else {
+      $('header nav').removeClass('inbody');
+    }
+  });
+
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+
+
+
+$('.carousel').carousel({
+
+interval:false;
+
+});
+
+
+
+
+
+}
+
+
+ 
